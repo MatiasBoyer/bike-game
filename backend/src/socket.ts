e1db832f1f6a828162e38bce51b40f35ec0649e6
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-15 18:24:26 -0300

    (back) feat: registerplayerhandlers/registerroomhandlers

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -1,5 +1,12 @@
-import { Server } from "socket.io";
+import { Server, Socket } from "socket.io";
+import RegisterPlayerHandlers from "./sockets/player.socket";
+import RegisterRoomHandlers from "./sockets/room.socket";
 
 export default function init_socket(io: Server) {
+  const onConnection = (socket: Socket) => {
+    RegisterPlayerHandlers(io, socket);
+    RegisterRoomHandlers(io, socket);
+  };
 
+  io.on("connection", onConnection);
 }
---
+++ backend/src/sockets/room.socket.ts
@@ -0,0 +1,5 @@
+import { Server, Socket } from "socket.io";
+
+export default (io: Server, socket: Socket) => {
+
+};
