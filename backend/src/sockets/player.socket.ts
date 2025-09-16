a5c18a3e76144b8defd931be462517f6c4f86dad
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-16 01:55:47 -0300

    (back) feat: player update_direction

--- backend/src/sockets/player.socket.ts
+++ backend/src/sockets/player.socket.ts
@@ -1,12 +1,23 @@
-import { update_direction } from "interfaces/player.interface";
+import { Player, update_direction } from "../interfaces/player.interface";
 import { Server, Socket } from "socket.io";
 import schemas from "../schemas/player.schemas";
+import playerService from "../services/player.service";
 
 export default (io: Server, socket: Socket) => {
   const update_direction = (payload: update_direction, callback: Function) => {
-      const { error, value } = schemas.update_direction.validate(payload);
-      if(error) { return; }
-    };
+    const { error, value } = schemas.update_direction.validate(payload);
+    if (error) {
+      return;
+    }
+
+    try {
+      const player: Player = playerService.FindPlayer(socket);
+      player.currentDirection = [value.x, value.y];
+      callback({ success: true });
+    } catch {
+      callback({ success: false });
+    }
+  };
 
   socket.on("player:update_direction", update_direction);
 };
