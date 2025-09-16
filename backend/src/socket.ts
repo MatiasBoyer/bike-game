492f735f98b6152eac984b563fa3d6b620956e50
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-16 10:46:24 -0300

    (back) style: linting

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -5,9 +5,15 @@ import { v4 } from "uuid";
 
 export default function init_socket(io: Server) {
   const onConnection = (socket: Socket) => {
+    console.info(`[CONNECT] ${socket.id}`);
+
     socket.data.uuid = v4();
     RegisterPlayerHandlers(io, socket);
     RegisterRoomHandlers(io, socket);
+
+    socket.on("disconnect", () => {
+      console.info(`[DISCONNECT] ${socket.id}`);
+    });
   };
 
   io.on("connection", onConnection);
