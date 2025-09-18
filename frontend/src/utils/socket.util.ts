a0a085e8929f1486a5156f91dca04ee675afe839
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-17 22:16:35 -0300

    (front) fix: socket now uses global_socket

--- frontend/src/utils/socket.util.ts
+++ frontend/src/utils/socket.util.ts
@@ -1,9 +1,9 @@
-import { get_socket } from "../sockets/index.socket";
+import { global_socket } from "../sockets/index.socket";
 
 function emit(ev: string, payload: any) {
   return new Promise((resolve, reject) => {
     try {
-      get_socket().emit(ev, payload, (data: any) => {
+      global_socket.emit(ev, payload, (data: any) => {
         resolve(data);
       });
     } catch (err) {
