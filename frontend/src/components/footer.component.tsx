3cf5ad04d5ecc41ccca46aaddfece6892b6ce25d
Matías Boyer <matiasboyer7@hotmail.com>
2025-09-18 15:35:41 -0300

    (front) feat: footer now displays build # and branch

--- frontend/src/components/footer.component.tsx
+++ frontend/src/components/footer.component.tsx
@@ -1,10 +1,11 @@
 "use client";
 import Image from "next/image";
 import github_mark from "../assets/github-mark-white.svg";
+import config from "@/utils/config.util";
 
 export default function Footer() {
   return (
-    <footer className="w-full p-4 text-xs flex flex-row justify-between">
+    <footer className="w-full p-4 text-xs flex flex-row justify-between items-center">
       <span>
         made with ♥<br />
         matías boyer
@@ -14,6 +15,7 @@ export default function Footer() {
           <Image src={github_mark} alt="github-icon" width={24} height={24} />
         </a>
       </span>
+      <span className="text-right">build {config.build}<br/>{config.branch}</span>
     </footer>
   );
 }
